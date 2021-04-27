9401eb237b79d2aebfc2399d175521d3c61e60ae
Drew Burkhard <drew.burkhard@kellogg.northwestern.edu>
2021-04-27 12:13:56 -0500

    end of class 4.27.21

--- code-along/events.js
+++ code-along/events.js
@@ -1,19 +1,94 @@
-// Get a reference to the page title element from the DOM
-let pageTitleElement = document.querySelector(`.page-title`)
+// // Get a reference to the page title element from the DOM
+// let pageTitleElement = document.querySelector(`.page-title`)
 
-// Modify the page title element's contents
-pageTitleElement.innerHTML = `Movies to watch!`
+// // Modify the page title element's contents
+// pageTitleElement.innerHTML = `Movies to watch!`
 
-// Get a reference to the bulleted list of movies
-let movieList = document.querySelector(`.movies-to-watch`)
+// // Get a reference to the bulleted list of movies
+// let movieList = document.querySelector(`.movies-to-watch`)
 
-// Add an new list item to the bulleted list of movies
-movieList.insertAdjacentHTML(`beforeend`, `
-  <li>Spaceballs</li>
-`)
+// // Add an new list item to the bulleted list of movies
+// movieList.insertAdjacentHTML(`beforeend`, `
+//   <li>Spaceballs</li>
+// `)
 
-// Get a reference to the image
-let image = document.querySelector(`img`)
+// // Get a reference to the image
+// let image = document.querySelector(`img`)
 
-// Add the `border-pink-500` class to the image element
-image.classList.add(`border-pink-500`)
+// // Add the `border-pink-500` class to the image element
+// image.classList.add(`border-pink-500`)
+
+// Get a reference to the "click-me" button
+let clickMeButton = document.querySelector(`.click-me`)
+
+// Create an event listener
+clickMeButton.addEventListener(`click`, async function(event) {
+  // Get a reference to the page title element from the DOM
+  let pageTitleElement = document.querySelector(`.page-title`)
+
+  // Modify the page title element's contents
+  pageTitleElement.innerHTML = `Movies to watch!`
+  let movieList = document.querySelector(`.movies-to-watch`)
+})
+
+let addMovieLink = document.querySelector(`.add-movie`)
+
+// Create an event listener
+addMovieLink.addEventListener(`click`, async function(event) {
+  // Prevent the link's default behavior (aka go to Google)
+  event.preventDefault()
+
+  // Get a reference to the bulleted list of movies
+  let movieList = document.querySelector(`.movies-to-watch`)
+
+  // Add an new list item to the bulleted list of movies
+  movieList.insertAdjacentHTML(`beforeend`, `
+    <li>Spaceballs</li>
+  `)
+
+})
+
+let zoomImageButton = document.querySelector(`.zoom-image`)
+
+// Create an event listener for the clickMeButton
+zoomImageButton.addEventListener(`click`, async function(event) {
+  let image = document.querySelector(`img`)
+  image.classList.toggle(`w-96`)
+  image.classList.toggle(`w-full`)
+
+})
+
+let changeImageButton = document.querySelector(`.change-image`)
+
+// Create an event listener
+changeImageButton.addEventListener(`click`, async function(event) {
+  let image = document.querySelector(`img`)
+  image.setAttribute(`src`,`../images/grogu2.jpg`)
+})
+
+let sayHiButton = document.querySelector(`.say-hi`)
+
+sayHiButton.addEventListener(`click`, async function(event) {
+  event.preventDefault()
+  console.log(`the form was submitted`)
+  // Get a reference from the first name input field
+  let nameInput = document.querySelector(`#first-name`
+  )
+  // Get the value from the input field
+  let firstName = nameInput.value
+
+  // Get a  reference to the "greet" element
+  let greetElement = document.querySelector(`.greet`)
+
+  // First name should have value
+  if (firstName.length >0) {
+    
+    // Create a sentence to put in the "greet" element
+    let sentence = `Hi, ${firstName}!`
+    
+    //Set the "greet element's HTML to the sentence"
+    greetElement.innerHTML = sentence
+  } else {
+    greetElement.innerHTML = ``
+  }
+})
--- lab/week5.js
+++ lab/week5.js
@@ -15,26 +15,56 @@
 
 window.addEventListener('DOMContentLoaded', async function() {
   // Get a reference to the "get weather" button
-
+  let getWeatherButton = document.querySelector(`.get-weather`)
+  
   // When the "get weather" button is clicked:
+  getWeatherButton.addEventListener(`click`, async function(event) {
 
     // - Ignore the default behavior of the button
+    event.preventDefault()
 
     // - Get a reference to the element containing the user-entered location
-
-    // - Get the user-entered location from the element's value
+    let locationInput = document.querySelector(`#location`)
+    let numDaysInput = document.querySelector(`#days`)
+    
+    // - Get the user-entered location and number of days from the elements' values
+    let location = locationInput.value
+    let numDays = numDaysInput.value
 
     // - Check to see if the user entered anything; if so:
-
+    if (location.length > 0 && numDays.length > 0) {
       // - Construct a URL to call the WeatherAPI.com API
-
+      let url = `https://api.weatherapi.com/v1/forecast.json?key=c888d50f532441e1b7a153859212704&q=${location}&days=${numDays}`
+      
       // - Fetch the url, wait for a response, store the response in memory
+      let response = await fetch(url)
 
       // - Ask for the json-formatted data from the response, wait for the data, store it in memory
+      let json = await response.json()
 
       // - Write the json-formatted data to the JavaScript console
+      console.log(json)
 
       // - Store the interpreted location, current weather conditions, the forecast as three separate variables
+      let locationNew = `${json.location.name}, ${json.location.region}`
+      let weatherConditions = `${json.current.temp_f} and ${json.current.condition.text}`
+      let forecastArray = json.forecast
 
-      // - Continue the recipe yourself!
+      // Get a reference for the location title
+      let locationTitle = document.querySelector(`.current`)
+
+      // Appending HTML using adjacent HMTL function
+      locationTitle.innerHTML = `
+      <div class="text-center space-y-2">
+      <div class="font-bold text-3xl">Current Weather for ${locationNew}</div>
+      <div class="font-bold">
+        <img src="https://cdn.weatherapi.com/weather/64x64/day/116.png" class="inline-block">
+        <span class="temperature">${json.current.temp_f}</span>° 
+        and
+        <span class="conditions">${json.current.condition.text}</span>
+      </div>
+    </div>`
+    }
+
+  })
 })
